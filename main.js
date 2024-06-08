afa650abcf52bc283f39e7325656967bd4dc150b
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 16:44:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var guestList = ["Hammad Ali", "shehbaz", "sami"];
+for (var i = 0; i < guestList.length; i++) {
+    console.log("Dear mr", guestList[i], "It is our plesure to invite you to dinner,Thank you");
+}
---
+++ main.ts
@@ -0,0 +1,8 @@
+  let guestList :string [] = ["Hammad Ali","shehbaz","sami"];
+
+  for (let i = 0; i < guestList.length; i++){
+console.log("Dear mr",guestList[i],"It is our plesure to invite you to dinner,Thank you");
+
+  }
+
+  
